e1bb57fc87c383dc5f1ecc7a564ff89f2338a680
KimInteger <kjs20151240@gmail.com>
2024-06-11 09:29:52 +0900

    db초기화

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ const fs = require('node:fs');
 const qs = require('node:querystring');
 
 const path = require('node:path');
+
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('./Database/chinook.db');
