2211d0022e15bbde7a759168797b632b5fba63d2
KimInteger <kjs20151240@gmail.com>
2024-06-11 09:31:50 +0900

    server생성 및 포트에 연결

--- app.js
+++ app.js
@@ -9,3 +9,18 @@ const path = require('node:path');
 const sqlite3 = require('sqlite3').verbose();
 
 const db = new sqlite3.Database('./Database/chinook.db');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req,res)=>{
+  console.log(1);
+});
+
+server.listen(PORT,(err)=>{
+  if(err){
+    console.error(err);
+  } else {
+    console.log('server responsed');
+    console.log(`http://localhost:${PORT}`);
+  }
+});
