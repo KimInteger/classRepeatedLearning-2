3c029b3807ef520bd9305a1a715b9ff9cfaa4b11
KimInteger <kjs20151240@gmail.com>
2024-06-11 10:12:57 +0900

    server실행문 작성

--- app.js
+++ app.js
@@ -13,7 +13,47 @@ const db = new sqlite3.Database('./Database/chinook.db');
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req,res)=>{
-  console.log(1);
+  console.log(req.url);
+  if(req.method === 'GET'){
+    if(req.url === '/'){
+      fs.readFile('./public/index.html', (err,data)=>{
+        if(err) {
+          res.writeHead(500,{"Content-Type":"text/plain; charset=UTF-8"});
+          res.end("서버 자체 에러입니다.");
+          return;
+        } else {
+          res.writeHead(200,{"Content-Type":"text/html;charset=UTF-8"});
+          res.end(data);
+        }
+      });
+    } else if(req.url === '/script.js') {
+      fs.readFile('./public/script.js', (err,data)=>{
+        if(err){
+          res.writeHead(500,{"Content-Type":"text/plain; charset=UTF-8"});
+          res.end("서버 자체 에러입니다.");
+          return;
+        } else {
+          res.writeHead(200,{"Content-Type":"application/javascript; charset=UTF-8"});
+          res.end(data);
+        }
+      });
+    } else {
+      res.writeHead(404,{"Content-Type":"test/plain; charset=UTF-8"});
+      res.end('페이지를 찾을 수 없습니다.');
+    }
+  } else if(req.method === 'POST'){
+    let body = '';
+    req.on('data', (chunk)=>{
+      body += chunk.toString();
+    });
+    req.on('end',()=>{
+      const parsedData = qs.parse(body);
+      console.log(parsedData);
+    });
+  } else {
+    res.writeHead(404,{"Content-Type" : "text/plain; charset=UTF-8"});
+    res.end('페이지를 찾을 수 없습니다.');    
+  }
 });
 
 server.listen(PORT,(err)=>{
